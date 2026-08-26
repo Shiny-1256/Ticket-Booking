6975e7ebcf5569142348fe0931e82a9e9227c569
Shiny-1256 <shiny1256u@gmail.com>
2026-08-26 21:16:08 +0530

    Change allowedHosts to true in Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['spectacular-wisdom-production-91f4.up.railway.app'],
+    allowedHosts: true,
   },
 })
